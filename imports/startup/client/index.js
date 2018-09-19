f7b95b1530748e114036025aa16df5753f4527c6
gagpmr <absolutely2great@gmail.com>
2018-09-20 01:38:11 +0530

    apollo added

---
+++ imports/startup/client/index.js
@@ -0,0 +1,24 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// import ApolloClient from "apollo-client";
+// import { ApolloProvider } from "react-apollo";
+// import App from "/imports/ui/layouts/app";
+// import { Bert } from "meteor/themeteorchef:bert";
+// import { Meteor } from "meteor/meteor";
+// import React from "react";
+// import { meteorClientConfig } from "meteor/apollo";
+// import { render } from "react-dom";
+
+// Bert.defaults.style = "growl-bottom-right";
+// Bert.defaults.hideDelay = 1500;
+
+// const client = new ApolloClient(meteorClientConfig());
+
+// Meteor.startup(() => {
+//   render(
+//     <ApolloProvider client={client}>
+//       <App />
+//     </ApolloProvider>,
+//     document.getElementById("react-root")
+//   );
+// });
---
+++ imports/startup/server/index.js
@@ -0,0 +1,23 @@
+// // import './browser-policy';
+// import "./accounts";
+// import "./fixtures";
+// import "./api";
+
+// import { createApolloServer } from "meteor/apollo";
+// import { makeExecutableSchema } from "graphql-tools";
+// import { resolvers } from "/imports/server/resolvers";
+// import { typeDefs } from "/imports/server/schema";
+
+// // const cors = require("cors");
+
+// const context = {};
+
+// const schema = makeExecutableSchema({
+//   typeDefs,
+//   resolvers
+// });
+
+// createApolloServer({
+//   schema,
+//   context
+// });
